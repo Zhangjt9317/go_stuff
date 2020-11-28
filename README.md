46b64a0e616b002c62e21637490b81d2b2b28794
Jingtian Zhang <jtz9317@gmail.com>
2020-11-28 04:26:13 -0800

    go content

---
+++ README.md
@@ -0,0 +1,4 @@
+## Some great resources for learning go
+
+-- many code snippets: https://gobyexample.com/url-parsing
+-- 
---
+++ http-clients.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"bufio"
+	"net/http"
+)
+
+// Issue an HTTP GET request to a server.
+//http.Get is a convenient shortcut around creating an http.Client object and calling its Get method;
+//it uses the http.DefaultClient object which has useful default settings.
+func main(){
+	resp, err := http.Get("http://gobyexample.com")
+	if err != nil{
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	fmt.Println("Response status: ", resp.Status)
+
+	scanner := bufio.NewScanner(resp.Body)
+
+	for i := 0; scanner.Scan() && i < 5; i++ {
+		fmt.Println(scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+}
